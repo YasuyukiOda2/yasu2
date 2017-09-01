ce69279ea9c6277f11d139a873fa74d53ec5bcca
YasuyukiOda2 <yasuyuki.oda@roboken2.com>
2017-09-01 05:52:50 +0000

    a

--- pdf-table-extract-master/pdf-table-extract-master/example/test_to_pandas.py
+++ pdf-table-extract-master/pdf-table-extract-master/example/test_to_pandas.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import pdftableextract as pdf
 
-pages = ["1"]
-cells = [pdf.process_page("kouku1.pdf",p) for p in pages]
+pages = ["2"]
+cells = [pdf.process_page("usca.pdf",p) for p in pages]
 
 #flatten the cells structure
 cells = [item for sublist in cells for item in sublist ]
@@ -10,6 +10,8 @@ cells = [item for sublist in cells for item in sublist ]
 #without any options, process_page picks up a blank table at the top of the page.
 #so choose table '1'
 li = pdf.table_to_list(cells, pages)[1]
+print(li)
+print(len(li))
 
 #li is a list of lists, the first line is the header, last is the footer (for this table only!)
 #column '0' contains store names
@@ -17,4 +19,4 @@ li = pdf.table_to_list(cells, pages)[1]
 #data is row '2' through '-1'
 
 data =pd.DataFrame(li[2:-1], columns=li[1], index=[l[0] for l in li[2:-1]])
-data.to_csv('kouku1_sample1.csv')
+data.to_csv('usca_sample1.csv')
