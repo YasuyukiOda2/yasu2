1539fb31cc54420c030d33a7e12bafe1d137db8b
YasuyukiOda2 <yasuyuki.oda@roboken2.com>
2017-09-26 08:52:59 +0000

    huhuhu

--- python_scraping2/python_scraping2/selenium_middlewares.py
+++ python_scraping2/python_scraping2/selenium_middlewares.py
@@ -1,28 +1,38 @@
 import os.path
+import time
+import copy
 from urllib.parse import urlparse
 from scrapy.http import HtmlResponse
 from selenium.webdriver import PhantomJS
 from selenium.webdriver.common.keys import Keys
-import time
-from python_scraping2.utils import get_url, getExampleSentence, getExplainList
+from python_scraping2.utils import get_url, getExplainSentence, getExplainList
 
 driver = PhantomJS()
 words = ['for', 'print']
 HEADS = ['F', 'P']
-explain_list = []
 
 class SeleniumMiddleware(object):
     def process_request(self, request, spider):
-        request1 = request
-        driver.get(request.url)
-        driver.find_element_by_link_text('総索引').click()
+        #url_list = []
         explain_list = []
+        example_sentence_list = []
+        explain_list = []
+        request1 = copy.deepcopy(request)
+        driver.get(request1.url)
+        driver.find_element_by_link_text('総索引').click()
         for (H, w) in zip(HEADS, words):
             driver.find_element_by_link_text(H).click()
             url_now = driver.current_url
             url_list = get_url(url_now, w) #utils.get_url
-            example_sentense_list = getExampleSentence(request1, url_list, w) #utils.getExampleSentence
-            explain_list = getExplainList(words, example_sentense_list, explain_list) #utils.getExplainList
+            assert type(url_list) != 'NoneType', "url_list = NoneType object"
+            assert len(url_list) > 1, "url_list couldn't get"
+            example_sentence_list = getExplainSentence(
+                example_sentence_list, request1, url_list, w) #utils.getExplainSentence
+            explain_list_add = getExplainList(words, example_sentence_list) #utils.getExplainList
+            explain_list = explain_list.append(explain_list_add)
+            driver.get(request1.url)
+            driver.find_element_by_link_text('総索引').click()
+            example_sentence_list = []
         return explain_list
 
 #def searchKeyword():
--- python_scraping2/python_scraping2/utils.py
+++ python_scraping2/python_scraping2/utils.py
@@ -3,12 +3,14 @@ import lxml.html
 import readability
 import requests
 import re
-from urllib.parse.urljoin
+from urllib.parse import urljoin
 
 # ReadabilityのDEBUG/INFOレベルのログを表示しないようにする
 logging.getLogger('readability.readability').setLevel(logging.WARNING)
 
 def get_url(url, word):
+
+    url_list = []
     """
     キーワードに関する記述のあるurlを取得
     """
@@ -17,49 +19,71 @@ def get_url(url, word):
     r = requests.get(url)
     r.encoding = r.apparent_encoding
     text = r.text.strip()
+    print(text) #確認用
+    print(word) #確認用
 
     # 正規表現でリンクを抽出
     pattern1 = "<dt>\s*" + word + "\s*<dt/>"
     pattern1_1 = pattern1 + "\s*<dd><dl>\s*<dt>(.*)\s*</dt>"
-    pattern2 = 
-    if re.search(pattern1_1, text).group(1):
-        text1_1 = re.search(pattern1_1, text).group(1)
-        pattern1_1_1 = '<a href="([^"]*)">'
-        url_iterater = re.finditer(pattern1_1_1, text1_1)
-        url_list = []
-        for match_url in url_iterater:
-            url_list.append(match_url.group(1))
-        return url_list
+    pattern2 = "<dt>\s*" + word + "[()].*?\s*</dt>"
+    pattern2_1 = pattern2 + "\s*<dd><dl>\s*<dt>(.*)\s*</dt>"
+    pattern3 = '<dt>\s<a href="(.*?)">\s*' + word + '[()].*?\s*</a>'
+    if re.search(pattern3, text):
+        text3 = re.search(pattern3, text).group(1)
+        url_list = url_list.append(text3)
+    elif re.search(pattern1_1, text):
+        url_list = url_list.append(getUrlList(pattern1_1, text))
+        #text_href = re.search(pattern1_1, text).grop(1)
+        #pattern_href = '<a href="([^"]*)">'
+        #url_iterater = re.finditer(pattern_href, text_href)
+        #url_list = [match_url.group(1) for match_url in url_iterater]
+    elif re.search(pattern2_1, text):
+        url_list = url_list.append(getUrlList(pattern2_1, text))
+        #text_href = re.search(pattern2_1, text).grop(1)
+        #pattern_href = '<a href="([^"]*)">'
+        #url_iterater = re.finditer(pattern_href, text_href)
+        #url_list = [match_url.group(1) for match_url in url_iterater]
+    else:
+        assert len(url_list) > 1, "no pattern match"
+        print("ajognafngjsnojnjnbjnbjng")
+        print(url_list)
+    return url_list
 
-def getExplainSentence(request1, url_list, w):
+def getUrlList(pattern, text):
+    text_href = re.search(pattern, text).group(1)
+    pattern_href = '<a href="([^"]*)">'
+    url_iterater = re.finditer(pattern_href, text_href)
+    url_list = [match_url.group(1) for match_url in url_iterater]
+    return url_list
+
+def getExplainSentence(example_sentence_list, request1, url_list, w):
     for url in url_list:
-        url = urljoin(request1, url)
+        url = urljoin(request1.url, url)
         r = requests.get(url)
         r.encoding = r.apparent_encoding
         text = r.text.strip()
-        example_sentense_list = getExampleSentenceList(text)
-    return example_sentense_list
+        example_sentence_list_add = getExampleSentenceList(text)
+        example_sentence_list = example_sentence_list.append(example_sentence_list_add)
+    return example_sentence_list
 
 def getExampleSentenceList(text):
     compiled_pattern_pre = re.compile('<pre>.*?</pre>', re.DOTALL)
     example_sentence_list_html = compiled_pattern_pre.findall(text)
-    example_sentense_list = excludeHTML(example_sentence_list_html)
-    return example_sentense_list
+    example_sentence_list_add = excludeHTML(example_sentence_list_html)
+    return example_sentence_list_add
 
 def excludeHTML(example_sentence_list_html):
     pattern_html = re.compile("<[^>]*?>")
-    example_sentense_list = []
-    for sentence in example_sentence_list_html:
-        example_sentense_list.append(pattern_html.sub("", sentence))
-    return example_sentense_list
+    example_sentence_list_add = [
+        pattern_html.sub("", sentence) for sentence in example_sentence_list_html]
+    #for sentence in example_sentence_list_html:
+    #    example_sentense_list.append(pattern_html.sub("", sentence))
+    return example_sentence_list_add
 
-def getExplainList(words, example_sentense_list, explain_list):
+def getExplainList(words, example_sentence_list):
+    explain_list_add = []
     for s in example_sentence_list:
-        w_list = [] # w_listを初期化
-        for w in words:
-            if w not in s:
-                continue
-            w_list.append(w)
+        w_list = [w for w in words if w in s]
         if len(w_list) == len(words):
-            explain_list.append(s)
-    return explain_list
+            explain_list_add.append(s)
+    return explain_list_add
